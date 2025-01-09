f1fc42bd48504ed4cec287435a93ca98d0115afc
mohammed el badry <92531732+MohaElbadry@users.noreply.github.com>
2025-01-09 11:50:39 +0100

    Graphe Version

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,7 +4,7 @@ module org.main.ex {
     requires java.sql;
     requires mysql.connector.j;
 
-
-    opens org.main.ex to javafx.fxml;
-    exports org.main.ex;
+    opens ma.enset.exam_mohammed_elbadry.presentation.views to javafx.graphics, javafx.fxml;
+    opens ma.enset.exam_mohammed_elbadry.presentation.controllers to javafx.fxml;
+    exports ma.enset.exam_mohammed_elbadry.presentation.views;
 }
